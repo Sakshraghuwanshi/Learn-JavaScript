14edf6ff7a92fde165cadbd2eb6d3e66ff682be6
Sakshi Raghuwanshi <sakshiraghuwanshi108@gmail.com>
2022-05-24 01:54:24 +0530

    Convert kelvin into celsius

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('sakshi')
+// const kelvin temp 
+const kelvin = 0
+// const celsius temp
+const celsius = kelvin - 273
+// coverting celsius into fahrenheit 
+let fahrenheit = celsius * (9/5) + 32
+// updating decimal value into int 
+fahrenheit = Math.floor(fahrenheit)
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`)
+// convert celsius to newton scale
+let newton = celsius * (33/100)
+newton = Math.floor(newton)
+console.log(`The temperature is ${newton} degrees newton.`)
