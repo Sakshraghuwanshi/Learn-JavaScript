f63012d648c5b645caad358d548bebb423bb8ce2
Sakshi Raghuwanshi <sakshiraghuwanshi108@gmail.com>
2022-05-25 00:49:15 +0530

    implemented program dog years

---
+++ dogYears.js
@@ -0,0 +1,22 @@
+// created a variable named myAge 
+const myAge = 15;
+// created a variable named earlyYear
+let earlyYears = 2;
+earlyYears *= 10.5
+// for the first two year 
+let laterYears = myAge - 2;
+//  calculate the no of dog years
+laterYears *= 4
+console.log(`earlyYears: ${earlyYears}`)
+console.log(`laterYears: ${laterYears}`)
+// to cal dog years 
+let myAgeInDogYears = earlyYears + laterYears
+let myName = "SSSSSSSSSSSaksi".toLowerCase()
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`); 
+
+
+
+
+
+
+
